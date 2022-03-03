8e052d454771efa2a41b31ec5e8543276f39b5c6
Yosefelwawi <yosef.elwawi@gmail.com>
2022-03-03 20:36:08 +0100

    Filled Database with data

--- U2/database.js
+++ U2/database.js
@@ -1,39 +1,21 @@
 // Our database of dogs
 let database = [
+   {
+        id: 0,
+        name: "Hummus",
+        region: "Middle East",
+        country: "Palestine",
+        main: "Chickpeas",
+        sides: "Bread or Olives"
+    },
     {
         id: 1,
-        name: "Arya",
-        breed: "Husky",
-        age: 5
-    },
-    {
-        id: 2,
-        name: "Charlie",
-        breed: "Pudel",
-        age: 3
-    },
-    {
-        id: 3,
-        name: "Erik",
-        breed: "Shiba",
-        age: 3
-    },
-    {
-        id: 4,
-        name: "Sebbe",
-        breed: "Husky",
-        age: 5
-    },
-    {
-        id: 5,
-        name: "Billie",
-        breed: "French Bulldog",
-        age: 8
-    },
-    {
-        id: 6,
-        name: "Alfred",
-        breed: "Border Collie",
-        age: 5
+        name: "Dolma",
+        region: "Middle East",
+        country: "Turkey",
+        main: "Grape Leaves",
+        sides: "Yoghurt"
     }
+ 
+  
 ];
